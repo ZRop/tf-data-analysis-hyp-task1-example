19728da7a5e2d2db8c06c8fa2d1dc9278b27b11f
ZRop <90032285+ZRop@users.noreply.github.com>
2023-04-12 20:14:40 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,14 +1,38 @@
-import pandas as pd
 import numpy as np
+import plotly.graph_objects as go
+import pandas as pd
+from scipy.stats import norm
+from scipy.stats import ttest_ind
+import random
 
+chat_id = 672508499 # Ваш chat ID, не меняйте название переменной
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
-
-def solution(x_success: int, 
-             x_cnt: int, 
+def solution(x_success: int, #покупка
+             x_cnt: int, #Заявка
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+
+    data1 = [0]*x_cnt
+    data2 = [0]*y_cnt
+
+    for i in range(x_success):
+        idx = random.randint(0, x_cnt-1)
+        while data1[idx] == 1:
+            idx = random.randint(0, x_cnt-1)
+        data1[idx] = 1
+
+    for i in range(y_success):
+        idx = random.randint(0, y_cnt-1)
+        while data2[idx] == 1:
+            idx = random.randint(0, y_cnt-1)
+        data2[idx] = 1
+
+    ttest1 = ttest_ind(data1,data2,equal_var=False)
+    print(ttest1)
+
+    if ttest1[1] < 0.1:
+        return False
+    else:
+        return True
+    
+    
